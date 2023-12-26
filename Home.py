658b62989ee8a0a4b8ca4ea2f167a39b7b88cf84
wakayama-fair <154740090+wakayama-fair@users.noreply.github.com>
2023-12-26 20:44:23 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+# 利用可能な言語を定義
+st.title("商品紹介ページ")
+st.write("🇯🇵 サイドバーから言語を選択してください。")
+st.write("---") 
+st.title("Products Page")
+st.write("🇺🇸 Select your language from the sidebar.")
+st.write("---") 
+st.title("产品页面")
+st.write("🇨🇳 从侧边栏选择语言。")
+st.write("---") 
+st.title("상품 소개 페이지")
+st.write("🇰🇷 사이드바에서 언어를 선택하세요.")
